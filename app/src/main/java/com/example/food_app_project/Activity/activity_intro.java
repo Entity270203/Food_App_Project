5b4055af5e19f6b8c6e4b94d9be71e6bf9a69a90
Entity270203 <nhoxphuc101@gmail.com>
2024-05-30 13:15:46 +0700

    init project

---
+++ app/src/main/java/com/example/food_app_project/Activity/activity_intro.java
@@ -0,0 +1,16 @@
+package com.example.food_app_project.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.food_app_project.R;
+
+public class activity_intro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
